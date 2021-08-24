19ab281cfbe9e750e60446ccb1e56ea4e3d837d6
shahhardik4599 <hardikshah_2000@yahoo.com>
2021-08-24 11:54:16 +0530

    adding const in html how to add js in html using react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+const name = "Hardik";
+const no = 10;
+
 ReactDOM.render(
   <div>
-    <h1>Hello world </h1>
-    <p>This is paragraph</p>
+    <h1>Hello {name} </h1>
+    <p>Your fav number is {no}</p>
   </div>,
   document.getElementById("root")
 );
